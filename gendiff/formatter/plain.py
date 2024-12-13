795bdec40570f5d7eb13d33da0f0560619b9c964
Ivan Gagarinov <dzencot@gmail.com>
2024-12-13 18:58:41 +0500

    add plain

--- gendiff/formatter/plain.py
+++ gendiff/formatter/plain.py
@@ -1,2 +1,52 @@
-def plain(tree):
-    return True
+def get_formatted_value(value):
+    if isinstance(value, dict):
+        return '[complex value]'
+    if isinstance(value, bool):
+        return 'true' if value else 'false'
+    if value == None:
+        return 'null'
+    return f"'{value}'"
+
+def plain(diff):
+    def inner(node, paths=[]):
+        node_type = node.get('type')
+        key = node.get('key')
+        new_paths = [*paths, key]
+
+        match node_type:
+            case 'added':
+                value = node.get('value')
+                property_name = '.'.join(new_paths)
+                formatted_value = get_formatted_value(value)
+
+                result = f"Property '{property_name}' was added with value: {formatted_value}"
+                return result
+            case 'removed':
+                value = node.get('value')
+                property_name = '.'.join(new_paths)
+
+                result = f"Property '{property_name}' was removed"
+                return result
+            case 'unchanged':
+                return None
+            case 'nested':
+                children = node.get('children')
+                prepared = map(lambda child: inner(child, new_paths), children)
+                filtered = filter(lambda item: item != None, prepared)
+                return '\n'.join(list(filtered))
+            case 'changed':
+                property_name = '.'.join(new_paths)
+
+                old_value = node.get('old_value')
+                formatted_old_value = get_formatted_value(old_value)
+
+                new_value = node.get('new_value')
+                formatted_new_value = get_formatted_value(new_value)
+
+                return f"Property '{property_name}' was updated. From {formatted_old_value} to {formatted_new_value}"
+        raise ValueError(f'Unknown type: {node_type}')
+
+    prepared = map(inner, diff)
+    filtered = filter(lambda item: item != None, prepared)
+    return '\n'.join(list(filtered))
+
--- tests/test_gnediff.py
+++ tests/test_gnediff.py
@@ -13,8 +13,20 @@ def get_fixture(name):
     return data
 
 def test_json():
-    expected = get_fixture('stylish_output').strip()
+    stylish_expected = get_fixture('stylish_output').strip()
+    plain_expected = get_fixture('plain_output').strip()
+    json_expected = get_fixture('json_output').strip()
+
     first_file_path = get_fixture_path('file1.json')
     second_file_path = get_fixture_path('file2.json')
 
-    assert gendiff(first_file_path, second_file_path, 'stylish') == expected
+    assert gendiff(first_file_path, second_file_path, 'stylish') == stylish_expected
+    assert gendiff(first_file_path, second_file_path, 'plain') == plain_expected
+    assert gendiff(first_file_path, second_file_path, 'json') == json_expected
+
+# def test_json():
+#     expected = get_fixture('stylish_output').strip()
+#     first_file_path = get_fixture_path('file1.json')
+#     second_file_path = get_fixture_path('file2.json')
+
+#     assert gendiff(first_file_path, second_file_path, 'stylish') == expected
